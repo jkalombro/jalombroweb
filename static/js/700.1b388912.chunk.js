3870f1bed3083890caed7231fa66976668f97257
jkalombro <jkalombro@users.noreply.github.com>
2022-06-18 16:38:55 +0000

    deploy: 058b409f3960d62346471d5226324b9eb08f580c

---
+++ static/js/700.1b388912.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjalombroweb=self.webpackChunkjalombroweb||[]).push([[700],{4700:function(s,e,n){n.r(e),n.d(e,{default:function(){return c}});n(2791);var r=n(184),c=function(){return(0,r.jsxs)("section",{className:"contact-page",children:[(0,r.jsx)("br",{}),(0,r.jsx)("br",{}),(0,r.jsx)("br",{}),(0,r.jsx)("br",{}),(0,r.jsx)("h1",{children:"CONTACT page is still under construction :)"})]})}}}]);
+//# sourceMappingURL=700.1b388912.chunk.js.map
