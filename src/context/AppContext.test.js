d6ae5349a735b6fd2c242ac9958c60e6cd8f782e
jkninja238 <jkninja238@gmail.com>
2022-06-24 13:07:12 +0800

    solved error in app context testing

--- src/context/AppContext.test.js
+++ src/context/AppContext.test.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { fireEvent } from "@testing-library/react";
-import { renderRouter } from '../utils/jest-context-helper';
-import Navbar from '../components/Navbar';
+import { fireEvent, act } from "@testing-library/react";
+import render from '../utils/jest-context-helper';
+import App from '../App';
 
 describe("AppContext test", () => {
-  it('initializes with /jalombroweb active menu', () => {
-    const { getByText } = renderRouter(<Navbar />);
-    const element = getByText(/home/i);
+  it('initializes with /jalombroweb active menu', async () => {
+    const { getByRole } = await act(async () => render(<App />));
+    const element = getByRole('button', {
+      name: /home/i
+    });
     expect(element).toHaveClass("btn-selected");
   });
 
   it('updates active menu to ABOUT ME when clicked', async () => {
-    const { getByText, findByText } = renderRouter(<Navbar />);
-    const element = getByText(/about me/i)
+    const { getByRole, findByRole } = await act(async () => render(<App />));
+    const element = getByRole('button', {
+      name: /about me/i
+    });
     await fireEvent.click(element);
-    const newBtn = await findByText(/about me/i);
+    const newBtn = await findByRole('button', {
+      name: /about me/i
+    });
     expect(newBtn).toHaveClass("btn-selected");
   });
 });
